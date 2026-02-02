89074b9293363fb4bdbc26729e89aadca0c57f16
jandyroach6 <jandyroachy@gmail.com>
2026-02-02 12:29:15 +0530

    Done day 1

---
+++ hospital-queue/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-xl">
+        Hospital Digital Queue System
+      </h1>
+    </div>
+  );
+}
